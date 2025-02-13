33d031d670e92c22367108eae718c41e28ab619b
HomeBodyDevil_Home_PC <uman9123@gmail.com>
2025-02-13 11:33:43 +0900

    추가 : Scene전환시의 Loading창

--- Capstone/Assets/Scripts/Managers/SceneManagerEX.cs
+++ Capstone/Assets/Scripts/Managers/SceneManagerEX.cs
@@ -1,12 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneManagerEX : MonoBehaviour
 {
+    [SerializeField] Image loadingPanel;
+    [SerializeField] TextMeshProUGUI loadingText;
+    [SerializeField] float fadeTime;
+
     public const int SCENE_COUNT = 3;
 
     public static Action OnSwitchSceneToIntro;
@@ -39,6 +45,8 @@ public class SceneManagerEX : MonoBehaviour
     private void Start()
     {      
         currentScene = Scenes.MapScene;
+
+        loadingPanel.gameObject.SetActive(false);
     }
 
     private void Update()
@@ -80,17 +88,18 @@ public class SceneManagerEX : MonoBehaviour
             //if (enemyInstance)          // 다시 Map으로 돌아갈 때 enemy 객체가 있다면, 제거해준다.
             //    Destroy(enemyInstance);
 
-            SceneManager.LoadScene("MapScene");
+            // 흐으음
+            //SceneManager.LoadScene("MapScene");           
+            //CameraManager.Instance().SetCamera(CameraManager.CameraKind.MapCamera);
+            //OnSwitchSceneToMap.Invoke();
 
-            CameraManager.Instance().SetCamera(CameraManager.CameraKind.MapCamera);
-
-            OnSwitchSceneToMap.Invoke();
+            StartCoroutine("WaitForLoadScene", "MapScene");
         }
 
         BattleManager.Instance().RemoveEnemy();
         BattleManager.Instance().HealToPlayer(1.0f);
 
-        UIManager.Instance().CurrentUIManager().OnCanclePanel();
+        //UIManager.Instance().CurrentUIManager().OnCanclePanel();
 
         PlayerMovement.OnMakePlayerCanMove.Invoke();
     }
@@ -103,17 +112,21 @@ public class SceneManagerEX : MonoBehaviour
 
             ChangePlayerTransform(true, currentScene);
 
-            SceneManager.LoadScene("BattleScene");
+            // 흐으음.
+            //SceneManager.LoadScene("BattleScene");
+            //CameraManager.Instance().SetCamera(CameraManager.CameraKind.BattleCamera);
 
-            CameraManager.Instance().SetCamera(CameraManager.CameraKind.BattleCamera);
+            //BattleManager.Instance().UpdateDropLists();
+            //BattleManager.Instance().SetEnemy();
 
-            BattleManager.Instance().UpdateDropLists();
-            BattleManager.Instance().SetEnemy();
+            //OnSwitchSceneToBattle.Invoke();
+            //loadingPanel.SetActive(false);
 
-            OnSwitchSceneToBattle.Invoke();
+            StartCoroutine("WaitForLoadScene", "BattleScene");
+            //Debug.Log("TESTING");
         }
 
-        UIManager.Instance().CurrentUIManager().OnCanclePanel();    // Panel들 다 없애는 용도?
+        //UIManager.Instance().CurrentUIManager().OnCanclePanel();    // Panel들 다 없애는 용도?
 
         PlayerMovement.OnMakePlayerCantMove.Invoke();
 
@@ -130,6 +143,84 @@ public class SceneManagerEX : MonoBehaviour
         //    //SceneManager.UnloadSceneAsync(currentScene);
         //    currentScene = "BattleScene";
         //}
+    }
+
+    IEnumerator WaitForLoadScene(string sceneName)
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(sceneName);
+        op.allowSceneActivation = false;
+
+        loadingPanel.color = new Color(loadingPanel.color.r, loadingPanel.color.g, loadingPanel.color.b, 0.0f);
+        loadingPanel.gameObject.SetActive(true);
+        yield return StartCoroutine("Fade", true);
+
+        UIManager.Instance().CurrentUIManager().OnCanclePanel();    // Panel들 다 없애는 용도?
+
+        int count = 0;
+        while(!op.isDone)
+        {
+            count++;
+            Debug.Log(count);
+            yield return null;
+
+            if (op.progress < 0.9f)
+            {
+                Debug.Log("Scene Loading");                
+            }
+            else
+            {
+                op.allowSceneActivation = true;
+
+                if (sceneName == "BattleScene")
+                {
+                    CameraManager.Instance().SetCamera(CameraManager.CameraKind.BattleCamera);
+
+                    BattleManager.Instance().UpdateDropLists();
+                    BattleManager.Instance().SetEnemy();
+
+                    OnSwitchSceneToBattle.Invoke();
+                }
+                else if (sceneName == "MapScene")
+                {
+                    CameraManager.Instance().SetCamera(CameraManager.CameraKind.MapCamera);
+                    OnSwitchSceneToMap.Invoke();
+                }
+                else
+                {
+                    Debug.Log("WTF?");
+                }
+
+                break;
+            }
+        }
+
+        Debug.Log("Finished");
+        yield return new WaitForSecondsRealtime(0.05f);
+
+        yield return StartCoroutine("Fade", false);
+        loadingPanel.gameObject.SetActive(false);
+    }
+
+    IEnumerator Fade(bool isIn)
+    {
+        float tick = 0;
+        while(tick < fadeTime)
+        {
+            //Debug.Log(tick);
+
+            yield return null;
+            tick += Time.deltaTime;
+            
+            float alpha = isIn ? Mathf.Lerp(0.0f, 1.0f, (tick / fadeTime)) : Mathf.Lerp(1.0f, 0.0f, (tick / fadeTime));
+            if (alpha >= 0.99)
+                alpha = 1.0f;
+            else if (alpha <= 0.01)
+                alpha = 0.0f;
+
+            Color newColor = new Color(loadingPanel.color.r, loadingPanel.color.g, loadingPanel.color.b, alpha);
+            loadingPanel.color = newColor;
+            loadingText.color = newColor;
+        }
     }
 
     public void ActivateMiniGamesPanel()
